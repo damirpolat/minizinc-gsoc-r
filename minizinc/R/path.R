ae7c221492d97d495108aeea21d022636be566fc
damirpolat <dpulatov@uwyo.edu>
2020-03-18 21:42:02 -0600

    one license warning left. others are solved

--- minizinc/R/path.R
+++ minizinc/R/path.R
@@ -1,19 +1,21 @@
-#' @title set_path
-#' 
-#' @description Set Minizinc path
-#' 
+#' @title Set Minizinc path
+#'
+#' @description
+#' Provide path to Minizinc installation.
+#'
 #' @param path Path to Minizinc executable
-#' 
+#'
 #' @export
 set_path = function(path) {
   .globals$minizinc_path = path
 }
 
 
-#' @title get_path
-#' 
-#' @description Get Minizinc path
-#' 
+#' @title Get Minizinc path
+#'
+#' @description
+#' Get path to Minizinc installation
+#'
 #' @export
 get_path = function() {
   return(.globals$minizinc_path)
